eebaa939ca00b894d9465a83792d234007f8c6d3
iotatheta09 <iotatheta09@gmail.com>
2026-05-07 19:29:58 +0530

    routes models added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userRouter from "./routes/user.routes.js";
+
 import cors from 'cors';
 
 import cookieParser from 'cookie-parser';
@@ -26,15 +26,32 @@ app.use(cookieParser());
 
 app.use(express.static('public'));
 
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 
 
-
-
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter);
 //first come to /api/v1/users then to userRouter and then to /register and then to registerUser controller function 
 // and then whole route is /api/v1/users/register and method is post and then we will get response from registerUser controller function
 
 //
+
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export {app};
